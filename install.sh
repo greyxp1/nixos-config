7979f3391cc3703cd04d7c655f81dfb2e1dd8322
Greyxp1 <greyxp999@gmail.com>
2026-04-04 18:03:22 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -15,13 +15,15 @@ git clone "https://github.com/$REPO.git" /tmp/nixos-install
 cd /tmp/nixos-install
 
 echo ">>> Setting target disk to $DISK in disko-config.nix..."
+# This ensures your disko-config uses the physical drive you passed as an argument
 sed -i "s|__TARGET_DISK__|$DISK|g" disko-config.nix
 
 echo ">>> Running disko-install..."
-# We use the full flake path and explicit flag for the disk
+# We use the direct executable path to ensure arguments are passed correctly
 sudo nix --experimental-features "nix-command flakes" \
   run github:nix-community/disko/latest -- \
   --mode disko-install \
   --flake ".#default" \
+  --write-efi-boot-entries \
   --yes-wipe-all-disks \
-  --argstr device "$DISK"
+  main "$DISK"
