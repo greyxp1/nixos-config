7e4366256558bf508eb628ceb1b1282fbfc0d2ac
Greyxp1 <greyxp999@gmail.com>
2026-04-05 06:28:06 -0400

    Update configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -3,25 +3,19 @@
     ./disko-config.nix
   ];
 
-  # Bootloader setup (Systemd-boot works on 99% of modern UEFI systems)
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
+  boot.loader.grub.enable = true;
+  boot.loader.grub.device = "/dev/nvme0n1";
 
-  # Networking
   networking.hostName = "nixos";
   networking.networkmanager.enable = true;
-
-  # Define your user account
   users.users.grey = {
     isNormalUser = true;
     extraGroups = [ "networkmanager" "wheel" ];
     initialPassword = "password";
   };
 
-  # Enable flakes by default
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
 
-  # Your Packages
   environment.systemPackages = with pkgs; [
     git
     vim
