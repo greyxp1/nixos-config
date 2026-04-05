7c5cc02dcc04e6e69ef4fb40d81482b90d468b4c
Greyxp1 <greyxp999@gmail.com>
2026-04-05 18:51:46 -0400

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -38,6 +38,7 @@
             home-manager.useGlobalPkgs = true;
             home-manager.useUserPackages = true;
             home-manager.users.grey = import ./home.nix;
+            home-manager.extraSpecialArgs = { inherit inputs; };
           }
         ];
       };
