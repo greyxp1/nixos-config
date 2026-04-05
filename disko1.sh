cedd4cc9bbdef1443309474d2d52ff5e44af8110
Greyxp1 <greyxp999@gmail.com>
2026-04-05 12:13:25 -0400

    Update disko1.sh

--- disko1.sh
+++ disko1.sh
@@ -5,4 +5,5 @@ sudo nix --experimental-features "nix-command flakes" run \
   --write-efi-boot-entries \
   --flake "github:greyxp1/nixos-config#nixos" \
   --disk nixos /dev/nvme0n1 \
-  --nix-upgrade-arguments "--no-write-lock-file"
+  --option warn-dirty false \
+  --option commit-lockfile-summary "false"
