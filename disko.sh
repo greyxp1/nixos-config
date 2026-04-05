ef15cda95c64035a5c1149a6f5ea6344f3ba94cb
Greyxp1 <greyxp999@gmail.com>
2026-04-05 12:37:48 -0400

    Update disko.sh

--- disko.sh
+++ disko.sh
@@ -1,25 +1,15 @@
 #!/usr/bin/env bash
-# 1. Exit immediately if a command fails
 set -e
 
-# 2. Use a temporary directory
-WORK_DIR=$(mktemp -d)
-echo "Working in $WORK_DIR..."
+git clone https://github.com/greyxp1/nixos-config.git /tmp/nixos-config
+cd /tmp/nixos-config
 
-# 3. Clone into the temp directory
-git clone https://github.com/greyxp1/nixos-config.git "$WORK_DIR"
-cd "$WORK_DIR"
-
-# 4. Run the installer
 sudo nix --experimental-features "nix-command flakes" run \
   'github:nix-community/disko/latest#disko-install' -- \
   --write-efi-boot-entries \
   --flake '.#nixos' \
   --disk nixos /dev/nvme0n1
 
-# 5. Clean up
-cd /
-sudo rm -rf "$WORK_DIR"
-
-# 6. Final destination
-cd /etc/nixos || echo "Note: /etc/nixos may not exist until you reboot into your new system."
+sudo mkdir -p /mnt/etc/nixos
+sudo cp -r /tmp/nixos-config/. /mnt/etc/nixos/
+sudo rm -rf /tmp/nixos-config
