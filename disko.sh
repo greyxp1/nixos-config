f36f6f8d60d33b6952537645d3d70751f0ab0878
Greyxp1 <greyxp999@gmail.com>
2026-04-05 11:37:55 -0400

    Create disko.sh

---
+++ disko.sh
@@ -0,0 +1,1 @@
+sudo nix --experimental-features "nix-command flakes" run 'github:nix-community/disko/latest#disko-install' -- --write-efi-boot-entries --flake github:greyxp1/nixos-config#nixos --disk nixos /dev/nvme0n1
