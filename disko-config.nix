9de277a82c68e6156924f0ae4e4089971f2d52a8
Greyxp1 <greyxp999@gmail.com>
2026-04-05 02:18:32 -0400

    Update disko-config.nix

--- disko-config.nix
+++ disko-config.nix
@@ -3,30 +3,49 @@
     disk = {
       main = {
         type = "disk";
-        device = lib.mkDefault "/dev/sda";
+        device = lib.mkDefault "/dev/sda"; #
         content = {
           type = "gpt";
           partitions = {
-            boot = {
-              size = "1M";
-              type = "EF02";
-            };
+            # For systemd-boot on UEFI
             ESP = {
               size = "1G";
-              type = "EF00";
+              type = "EF00"; #
               content = {
                 type = "filesystem";
-                format = "vfat";
+                format = "vfat"; #
                 mountpoint = "/boot";
                 mountOptions = [ "umask=0077" ];
               };
             };
+            # Btrfs partition for the OS
             root = {
               size = "100%";
               content = {
-                type = "filesystem";
-                format = "ext4";
-                mountpoint = "/";
+                type = "btrfs";
+                extraArgs = [ "-f" ]; # Override existing partition
+                subvolumes = {
+                  # The actual root (to be wiped in Tony's guide)
+                  "/root" = {
+                    mountpoint = "/";
+                    mountOptions = [ "compress=zstd" "noatime" ];
+                  };
+                  # Persistent user data
+                  "/home" = {
+                    mountpoint = "/home";
+                    mountOptions = [ "compress=zstd" "noatime" ];
+                  };
+                  # The Nix store (read-only system files)
+                  "/nix" = {
+                    mountpoint = "/nix";
+                    mountOptions = [ "compress=zstd" "noatime" ];
+                  };
+                  # Optional: persistence layer if you do the "wipe on boot"
+                  "/persist" = {
+                    mountpoint = "/persist";
+                    mountOptions = [ "compress=zstd" "noatime" ];
+                  };
+                };
               };
             };
           };
