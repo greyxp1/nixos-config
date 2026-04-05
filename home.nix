817cc619bc09775d05e4edb8d54dff48ef7302f3
Greyxp1 <greyxp999@gmail.com>
2026-04-05 18:38:46 -0400

    Update home.nix

--- home.nix
+++ home.nix
@@ -53,12 +53,15 @@
     enable = true;
   };
 
-  programs.niri.settings = {
-    input.keyboard.xkb.layout = "us,ua";
-    layout.gaps = 5;
-    binds = {
-      "Mod+Return".spawn-sh = lib.getExe pkgs.ghostty;
-      "Mod+Q".close-window = null;
+  programs.niri = {
+    enable = true;
+    settings = {
+      input.keyboard.xkb.layout = "us,ua";
+      layout.gaps = 5;
+      binds = {
+        "Mod+Return".spawn-sh = lib.getExe pkgs.ghostty;
+        "Mod+Q".close-window = null;
+      };
     };
   };
 
