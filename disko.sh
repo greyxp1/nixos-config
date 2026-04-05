628267dbdb26a1fab5ae40bac9ba50568c52995e
Greyxp1 <greyxp999@gmail.com>
2026-04-05 14:49:07 -0400

    Update disko.sh

--- disko.sh
+++ disko.sh
@@ -4,4 +4,5 @@ set -e
 sudo nix --experimental-features "nix-command flakes" run \
   'github:nix-community/disko/latest#disko-install' -- \
   --flake 'github:greyxp1/nixos-config#nixos' \
+  --write-efi-boot-entries \
   --disk nixos /dev/nvme0n1
