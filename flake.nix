b7172ecf5e884f05e8dd6015de949192dd3f5a63
Greyxp1 <greyxp999@gmail.com>
2026-04-07 15:02:28 -0400

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -11,66 +11,74 @@
 
   outputs = { self, nixpkgs, disko, wrappers, niri, ... }@inputs: {
     nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux"; # Required for nixosSystem
       specialArgs = { inherit inputs; };
+
       modules = [
         disko.nixosModules.disko
         ./configuration.nix
         niri.nixosModules.niri
 
-        ({ modulesPath, ... }: {
+        # Inline module to declare your system packages
+        ({ modulesPath, pkgs, ... }: {
           imports = [
             (modulesPath + "/installer/scan/not-detected.nix")
             (modulesPath + "/profiles/all-hardware.nix")
           ];
+
+          # Assign all wrapped and standard packages here
+          environment.systemPackages = [
+
+            (wrappers.wrapperModules.niri.apply {
+              inherit pkgs;
+              package = inputs.niri.packages.${pkgs.system}.niri-stable;
+              "niri/config.kdl".content = ''
+                input {
+                    keyboard { repeat-delay 200; repeat-rate 35; }
+                }
+                binds {
+                    "Super+Return" { spawn "ghostty"; }
+                    "Super+Q" { close-window; }
+                    "Super+Shift+E" { quit; }
+                }
+                layout {
+                    gutter 10
+                    default-column-width { proportion 0.5; }
+                }
+              '';
+            }).wrapper
+
+            (wrappers.wrapperModules.ghostty.apply {
+              inherit pkgs;
+              package = pkgs.ghostty; # Explicitly set the package to wrap
+              "ghostty/config".content = ''
+                theme = dark
+                font-family = "JetBrainsMono Nerd Font"
+                window-decoration = false
+                cursor-style = block
+              '';
+            }).wrapper
+
+            (wrappers.wrapperModules.git.apply {
+              inherit pkgs;
+              package = pkgs.git; # Explicitly set the package to wrap
+              "git/config".content = ''
+                [user]
+                  name = greyxp1
+                  email = greyxp999@gmail.com
+                [init]
+                  defaultBranch = main
+              '';
+            }).wrapper
+
+            # Standard packages
+            pkgs.vim
+            pkgs.curl
+            pkgs.tree
+            pkgs.bat
+          ];
         })
       ];
-
-      packages.x86_64-linux.default =
-
-        (wrappers.wrapperModules.niri.apply {
-          inherit pkgs;
-          package = inputs.niri.packages.${pkgs.stdenv.hostPlatform.system}.niri-stable;
-          "niri/config.kdl".content = ''
-            input {
-                keyboard { repeat-delay 200; repeat-rate 35; }
-            }
-            binds {
-                "Super+Return" { spawn "ghostty"; }
-                "Super+Q" { close-window; }
-                "Super+Shift+E" { quit; }
-            }
-            layout {
-                gutter 10
-                default-column-width { proportion 0.5; }
-            }
-          '';
-        }).wrapper
-
-        (wrappers.wrapperModules.ghostty.apply {
-          inherit pkgs;
-          "ghostty/config".content = ''
-            theme = dark
-            font-family = "JetBrainsMono Nerd Font"
-            window-decoration = false
-            cursor-style = block
-          '';
-        }).wrapper
-
-        (wrappers.wrapperModules.git.apply {
-          inherit pkgs;
-          "git/config".content = ''
-            [user]
-              name = greyxp1
-              email = greyxp999@gmail.com
-            [init]
-              defaultBranch = main
-          '';
-        }).wrapper
-
-      pkgs.vim
-      pkgs.curl
-      pkgs.tree
-      pkgs.bat
     };
   };
 }
