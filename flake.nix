6616fa85ea8c2099b1cb44f4379afa3d73a8b9be
Greyxp1 <greyxp999@gmail.com>
2026-04-07 14:57:57 -0400

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -13,63 +13,9 @@
     nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
       specialArgs = { inherit inputs; };
       modules = [
-        { nixpkgs.hostPlatform = "x86_64-linux"; }
         disko.nixosModules.disko
         ./configuration.nix
         niri.nixosModules.niri
-
-        ({ pkgs, ... }:
-          let
-            wm = inputs.wrappers.wrapperModules;
-          in {
-            environment.systemPackages = [
-
-              (wm.niri.apply {
-                inherit pkgs;
-                package = inputs.niri.packages.${pkgs.stdenv.hostPlatform.system}.niri-stable;
-                "niri/config.kdl".content = ''
-                  input {
-                      keyboard { repeat-delay 200; repeat-rate 35; }
-                  }
-                  binds {
-                      "Super+Return" { spawn "ghostty"; }
-                      "Super+Q" { close-window; }
-                      "Super+Shift+E" { quit; }
-                  }
-                  layout {
-                      gutter 10
-                      default-column-width { proportion 0.5; }
-                  }
-                '';
-              }).wrapper
-
-              (wm.ghostty.apply {
-                inherit pkgs;
-                "ghostty/config".content = ''
-                  theme = dark
-                  font-family = "JetBrainsMono Nerd Font"
-                  window-decoration = false
-                  cursor-style = block
-                '';
-              }).wrapper
-
-              (wm.git.apply {
-                inherit pkgs;
-                "git/config".content = ''
-                  [user]
-                    name = greyxp1
-                    email = greyxp999@gmail.com
-                  [init]
-                    defaultBranch = main
-                '';
-              }).wrapper
-
-              pkgs.vim
-              pkgs.curl
-              pkgs.tree
-              pkgs.bat
-            ];
-          })
 
         ({ modulesPath, ... }: {
           imports = [
@@ -78,6 +24,53 @@
           ];
         })
       ];
+
+      packages.x86_64-linux.default =
+
+        (wrappers.wrapperModules.niri.apply {
+          inherit pkgs;
+          package = inputs.niri.packages.${pkgs.stdenv.hostPlatform.system}.niri-stable;
+          "niri/config.kdl".content = ''
+            input {
+                keyboard { repeat-delay 200; repeat-rate 35; }
+            }
+            binds {
+                "Super+Return" { spawn "ghostty"; }
+                "Super+Q" { close-window; }
+                "Super+Shift+E" { quit; }
+            }
+            layout {
+                gutter 10
+                default-column-width { proportion 0.5; }
+            }
+          '';
+        }).wrapper
+
+        (wrappers.wrapperModules.ghostty.apply {
+          inherit pkgs;
+          "ghostty/config".content = ''
+            theme = dark
+            font-family = "JetBrainsMono Nerd Font"
+            window-decoration = false
+            cursor-style = block
+          '';
+        }).wrapper
+
+        (wrappers.wrapperModules.git.apply {
+          inherit pkgs;
+          "git/config".content = ''
+            [user]
+              name = greyxp1
+              email = greyxp999@gmail.com
+            [init]
+              defaultBranch = main
+          '';
+        }).wrapper
+
+      pkgs.vim
+      pkgs.curl
+      pkgs.tree
+      pkgs.bat
     };
   };
 }
