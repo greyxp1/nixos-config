8bf34c804555fccb8a6278817c4a868992201ec2
Greyxp1 <greyxp999@gmail.com>
2026-04-07 14:00:39 -0400

    Update configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -31,17 +31,7 @@
     openFirewall = true;
   };
 
-  programs.niri.enable = true;
-  programs.niri.package = inputs.niri.packages.${pkgs.stdenv.hostPlatform.system}.niri-stable;
   services.displayManager.ly.enable = true;
-
-  environment.systemPackages = with pkgs; [
-    git
-    vim
-    curl
-    tree
-    bat
-  ];
 
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
   system.stateVersion = "23.11";
