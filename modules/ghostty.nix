bb049f7850e8d0efc5b1c26304747eccbe8cb79f
Greyxp1 <greyxp999@gmail.com>
2026-04-08 10:13:52 -0400

    Update ghostty.nix

--- modules/ghostty.nix
+++ modules/ghostty.nix
@@ -1,34 +1,13 @@
-{ inputs, ... }: {
-  perSystem = { pkgs, ... }: {
-    packages.ghostty-custom = inputs.wrappers.wrappers.ghostty.wrap {
-      inherit pkgs;
-      settings = {
-        theme = "dark";
-        font-family = "JetBrainsMono Nerd Font";
-        window-decoration = false;
-        cursor-style = "block";
-      };
-    };
-  };
-}
-
-{ inputs, ... }:
+{ inputs, pkgs, ... }:
 {
-  imports = [
-    (inputs.wrappers.lib.mkInstallModule {
-      name  = "ghostty";
-      value = inputs.wrappers.lib.wrapperModules.ghostty;
-    })
-  ];
-
-  wrappers.ghostty = {
-    enable = true;
-    settings = {
-      user = {
-        name  = "greyxp1";
-        email = "greyxp999@gmail.com";
+  environment.systemPackages = [
+    (inputs.ghosttyWrappers.wrappers.ghostty.wrap ({ inherit pkgs; ... }: {
+      settings = {
+        theme                = "dark";
+        font-family          = "JetBrainsMono Nerd Font";
+        window-decoration    = false;
+        cursor-style         = "block";
       };
-      init.defaultBranch = "main";
-    };
-  };
+    }))
+  ];
 }
