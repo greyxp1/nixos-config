97a22c330e2e4226338c05c20d4adeabf6af2156
Greyxp1 <greyxp999@gmail.com>
2026-04-09 17:12:22 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,13 +8,29 @@ rm -rf nixos-config
 git clone https://github.com/greyxp1/nixos-config.git
 cd nixos-config
 
+echo "--------------------------------------------------------------------------------"
+echo "Detected the following devices:"
+echo
+
+i=0
+for device in $(sudo fdisk -l | grep "^Disk /dev" | awk "{print \$2}" | sed "s/://"); do
+    echo "[$i] $device"
+    i=$((i+1))
+    DEVICES[$i]=$device
+done
+
+echo
+read -p "Which device do you wish to install on? " DEVICE
+
+DEV=${DEVICES[$(($DEVICE+1))]}
+
 # 3. Disk Setup
 echo "Running Disko..."
 sudo nix --experimental-features "nix-command flakes" run \
   github:nix-community/disko/latest -- --mode destroy,format,mount \
   --yes-wipe-all-disks \
   ./disko-config.nix \
-  --argstr device /dev/nvme0n1
+  --argstr device $DEV
 
 # 3. Swap Setup
 echo "Setting up swap..."
