4f7a2feb1fd307ea10d91c4acacbf563d35ee732
Greyxp1 <greyxp999@gmail.com>
2026-04-09 17:16:05 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -9,20 +9,35 @@ git clone https://github.com/greyxp1/nixos-config.git
 cd nixos-config
 
 echo "--------------------------------------------------------------------------------"
-echo "Detected the following devices:"
+echo "Detected the following physical disks:"
 echo
 
+# Use lsblk -d to show only full disks, excluding partitions and loop devices
 i=0
-for device in $(sudo fdisk -l | grep "^Disk /dev" | awk "{print \$2}" | sed "s/://"); do
-    echo "[$i] $device"
+declare -A DEVICES
+while read -r dev; do
+    echo "[$i] /dev/$dev"
+    DEVICES[$i]="/dev/$dev"
     i=$((i+1))
-    DEVICES[$i]=$device
-done
+done < <(lsblk -dn -o NAME,TYPE | grep disk | awk '{print $1}')
 
 echo
-read -p "Which device do you wish to install on? " DEVICE
+# Force read from the terminal device (/dev/tty)
+read -p "Which device do you wish to install on? " CHOICE < /dev/tty
 
-DEV=${DEVICES[$(($DEVICE+1))]}
+DEV=${DEVICES[$CHOICE]}
+
+if [ -z "$DEV" ]; then
+    echo "Invalid selection. Exiting."
+    exit 1
+fi
+
+echo "Selected: $DEV"
+read -p "Confirm formatting $DEV? (y/n): " CONFIRM < /dev/tty
+if [[ ! "$CONFIRM" =~ ^[Yy]$ ]]; then
+    echo "Aborted."
+    exit 1
+fi
 
 # 3. Disk Setup
 echo "Running Disko..."
@@ -30,9 +45,9 @@ sudo nix --experimental-features "nix-command flakes" run \
   github:nix-community/disko/latest -- --mode destroy,format,mount \
   --yes-wipe-all-disks \
   ./disko-config.nix \
-  --argstr device $DEV
+  --argstr device "$DEV"
 
-# 3. Swap Setup
+# 4. Swap Setup
 echo "Setting up swap..."
 sudo fallocate -l 4G /mnt/swapfile
 sudo chmod 600 /mnt/swapfile
