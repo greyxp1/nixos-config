13097f4c209ccd16382f56422652d8d4daea7a90
Greyxp1 <greyxp999@gmail.com>
2026-04-09 04:21:27 -0400

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -27,7 +27,7 @@
 
         ({ pkgs, ... }: {
           nixpkgs.overlays = [ nix-cachyos-kernel.overlays.default ];
-          boot.kernelPackages = pkgs.cachyosKernels.linuxPackages-cachyos-latest;
+          boot.kernelPackages = pkgs.cachyosKernels.linuxPackages-cachyos-lto;
 
           environment.systemPackages = with pkgs; [
             vim
