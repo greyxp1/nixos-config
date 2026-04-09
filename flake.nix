3cf9fc47dfd64b4f739d3155c4465fa7b1988677
Greyxp1 <greyxp999@gmail.com>
2026-04-09 04:25:19 -0400

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -26,8 +26,17 @@
         ./modules/noctalia-shell.nix
 
         ({ pkgs, ... }: {
-          nixpkgs.overlays = [ nix-cachyos-kernel.overlays.default ];
-          boot.kernelPackages = pkgs.cachyosKernels.linuxPackages-cachyos-lts;
+          nixpkgs.overlays = [ nix-cachyos-kernel.overlays.pinned ];
+          boot.kernelPackages = pkgs.cachyosKernels.linuxPackages-cachyos-latest;
+
+          nix.settings.substituters = [
+            "https://attic.xuyh0120.win/lantian"
+            "https://cache.garnix.io"
+          ];
+          nix.settings.trusted-public-keys = [
+            "lantian:EeAUQ+W+6r7EtwnmYjeVwx5kOGEBpjlBfPlzGlTNvHc="
+            "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
+          ];
 
           environment.systemPackages = with pkgs; [
             vim
