dbaf243ccbcc8847e4397ae9a67614c764e5145f
Greyxp1 <greyxp999@gmail.com>
2026-04-09 04:42:53 -0400

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -27,16 +27,21 @@
 
         ({ pkgs, ... }: {
           nixpkgs.overlays = [ nix-cachyos-kernel.overlays.pinned ];
-          boot.kernelPackages = pkgs.cachyosKernels.linuxPackages-cachyos-latest;
+          boot.kernelPackages = pkgs.cachyosKernels.linuxPackages-cachyos-lts;
 
-          nix.settings.substituters = [
-            "https://attic.xuyh0120.win/lantian"
-            "https://cache.garnix.io"
-          ];
-          nix.settings.trusted-public-keys = [
-            "lantian:EeAUQ+W+6r7EtwnmYjeVwx5kOGEBpjlBfPlzGlTNvHc="
-            "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
-          ];
+          nix.settings = {
+            trusted-users = [ "root" "@wheel" ];
+
+            substituters = [
+              "https://attic.xuyh0120.win/lantian"
+              "https://cache.garnix.io"
+            ];
+
+            trusted-public-keys = [
+              "lantian:EeAUQ+W+6r7EtwnmYjeVwx5kOGEBpjlBfPlzGlTNvHc="
+              "cache.garnix.io:CTfPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
+            ];
+          };
 
           environment.systemPackages = with pkgs; [
             vim
