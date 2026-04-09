89342069909621a38259c07a59de08421d309cf4
Greyxp1 <greyxp999@gmail.com>
2026-04-08 22:21:07 -0400

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -5,15 +5,15 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     nix-cachyos-kernel.url = "github:xddxdd/nix-cachyos-kernel/release";
     wrappers.url = "github:BirdeeHub/nix-wrapper-modules";
+    wrappers.inputs.nixpkgs.follows = "nixpkgs";
     ghosttyWrappers.url = "github:nouritsu/nix-wrapper-modules/ghostty";
     ghosttyWrappers.inputs.nixpkgs.follows = "nixpkgs";
-    wrappers.inputs.nixpkgs.follows = "nixpkgs";
     disko.url = "github:nix-community/disko";
     disko.inputs.nixpkgs.follows = "nixpkgs";
   };
 
   outputs = inputs@{ self, nixpkgs, nix-cachyos-kernel, ... }: {
-    nixosConfigurations.nixos = inputs.nixpkgs.lib.nixosSystem {
+    nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       specialArgs = { inherit inputs self; };
       modules = [
@@ -26,10 +26,17 @@
         ./modules/noctalia-shell.nix
 
         ({ pkgs, ... }: {
+          nixpkgs.overlays = [ nix-cachyos-kernel.overlays.pinned ];
+          boot.kernelPackages = pkgs.cachyosKernels.linuxPackages-cachyos-latest;
 
-          nixpkgs.overlays = [
-              nix-cachyos-kernel.overlays.default
-            ];
+          nix.settings.substituters = [
+            "https://attic.xuyh0120.win/lantian"
+            "https://cache.garnix.io"
+          ];
+          nix.settings.trusted-public-keys = [
+            "lantian:EeAUQ+W+6r7EtwnmYjeVwx5kOGEBpjlBfPlzGlTNvHc="
+            "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
+          ];
 
           environment.systemPackages = with pkgs; [
             vim
