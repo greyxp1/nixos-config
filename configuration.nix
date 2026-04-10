e9a98f68e0f40cec0f8cd41e49a435ec5dda92fe
Greyxp1 <greyxp999@gmail.com>
2026-04-10 14:00:17 -0400

    Update configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -5,7 +5,7 @@ let
 in {
   boot.loader = lib.mkMerge [
     (lib.mkIf isUEFI {
-      systemd-boot.enable      = true;
+      systemd-boot.enable      = lib.mkForce false;
       efi.canTouchEfiVariables = true;
     })
     (lib.mkIf (!isUEFI) {
