01e228f9d7daa8f0c7a3b2cb1e0d1872b424b885
Greyxp1 <greyxp999@gmail.com>
2026-04-10 02:50:08 -0400

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -2,14 +2,12 @@
   description = "Modular Wrapper Flake";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    nix-cachyos-kernel.url = "github:xddxdd/nix-cachyos-kernel/release";
-    wrappers.url = "github:BirdeeHub/nix-wrapper-modules";
+    nixpkgs.url             = "github:nixos/nixpkgs/nixos-unstable";
+    nix-cachyos-kernel.url  = "github:xddxdd/nix-cachyos-kernel/release";
+    wrappers.url            = "github:BirdeeHub/nix-wrapper-modules";
     wrappers.inputs.nixpkgs.follows = "nixpkgs";
-    ghosttyWrappers.url = "github:nouritsu/nix-wrapper-modules/ghostty";
+    ghosttyWrappers.url     = "github:nouritsu/nix-wrapper-modules/ghostty";
     ghosttyWrappers.inputs.nixpkgs.follows = "nixpkgs";
-    disko.url = "github:nix-community/disko";
-    disko.inputs.nixpkgs.follows = "nixpkgs";
   };
 
   outputs = inputs@{ self, nixpkgs, nix-cachyos-kernel, ... }: {
@@ -17,8 +15,8 @@
       system = "x86_64-linux";
       specialArgs = { inherit inputs self; };
       modules = [
-        inputs.disko.nixosModules.disko
-        ./disko-config.nix
+        ./hardware-configuration.nix
+        ./bootloader.nix
         ./configuration.nix
         ./modules/git.nix
         ./modules/niri.nix
@@ -31,24 +29,17 @@
 
           nix.settings = {
             trusted-users = [ "root" "@wheel" ];
-
             substituters = [
               "https://attic.xuyh0120.win/lantian"
               "https://cache.garnix.io"
             ];
-
             trusted-public-keys = [
               "lantian:EeAUQ+W+6r7EtwnmYjeVwx5kOGEBpjlBfPlzGlTNvHc="
               "cache.garnix.io:CTfPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
             ];
           };
 
-          environment.systemPackages = with pkgs; [
-            vim
-            curl
-            tree
-            bat
-          ];
+          environment.systemPackages = with pkgs; [ vim curl tree bat ];
         })
       ];
     };
