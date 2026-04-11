97047cb2739be5da9b6714f212cfd0d5fc91f25a
Greyxp1 <greyxp999@gmail.com>
2026-04-11 05:15:32 -0400

    Removed the quiet flag from nixos-install

--- install.sh
+++ install.sh
@@ -123,7 +123,6 @@ sudo nixos-install \
   --root /mnt \
   --flake ".#$FLAKE_ATTR" \
   --no-root-passwd \
-  --quiet \
   --option substituters "https://cache.nixos.org https://attic.xuyh0120.win/lantian https://cache.garnix.io" \
   --option trusted-public-keys "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY= lantian:EeAUQ+W+6r7EtwnmYjeVwx5kOGEBpjlBfPlzGlTNvHc= cache.garnix.io:CTfPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
 
