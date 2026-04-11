5dee98e193353dd68f9c1dcd6b30c7a818231ff3
Greyxp1 <greyxp999@gmail.com>
2026-04-11 02:54:29 -0400

    Added helium to list of outputs and updated flake.lock

--- flake.nix
+++ flake.nix
@@ -18,7 +18,7 @@
     };
   };
 
-  outputs = inputs@{ self, nixpkgs, nix-cachyos-kernel, lanzaboote, ... }: {
+  outputs = inputs@{ self, nixpkgs, nix-cachyos-kernel, lanzaboote, helium, ... }: {
     nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       specialArgs = { inherit inputs self; };
