a4762e678891e27f31ea998cb361f4f822409e0e
Greyxp1 <greyxp999@gmail.com>
2026-04-12 22:22:56 -0400

    s

--- modules/noctalia/noctalia-shell.nix
+++ modules/noctalia/noctalia-shell.nix
@@ -9,6 +9,7 @@
 
     wrappers.noctalia-shell = {
       enable = true;
+      outOfStoreConfig = "~/.config/noctalia";
       settings = (builtins.fromJSON (builtins.readFile ./noctalia.json));
       colors   = (builtins.fromJSON (builtins.readFile ./colors.json));
       plugins  = (builtins.fromJSON (builtins.readFile ./plugins.json));
