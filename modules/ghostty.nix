d493e7ec6b57736118db0926e461861ea8df3224
Greyxp1 <greyxp999@gmail.com>
2026-04-14 00:48:03 -0400

    Update ghostty.nix

--- modules/ghostty.nix
+++ modules/ghostty.nix
@@ -4,14 +4,30 @@
       (inputs.ghosttyWrappers.wrappers.ghostty.wrap {
         inherit pkgs;
         settings = {
-          background           = "1e1e2e";
-          foreground           = "cdd6f4";
-          cursor-color         = "f5e0dc";
-          selection-background = "585b70";
-          selection-foreground = "cdd6f4";
-          font-family          = "JetBrainsMono Nerd Font";
-          window-decoration    = false;
-          cursor-style         = "block";
+
+          theme = "Ghostty Default Style Dark";
+          background-opacity = 0.5;
+          window-decoration = false;
+
+          clipboard-read = "allow";
+          mouse-hide-while-typing = true;
+
+          selection-background = "ffffff";
+          selection-foreground = "282c34";
+
+          cursor-color = "ffffff";
+          cursor-text = "353a44";
+          cursor-style = "block";
+
+          font-family = "JetBrainsMono Nerd Font";
+
+          palette = [
+            "1=#cc6566"
+            "2=#b6bd68"
+            "4=#82a2be"
+            "7=#c4c8c6"
+            "10=#b9ca4b"
+          ];
         };
       })
     ];
