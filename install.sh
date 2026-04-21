09c1569a304ec0b128f3844519e0080e8242e6de
NixOS Installer <installer@nixos>
2026-04-21 07:50:59 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -134,7 +134,7 @@ sudo nixos-install \
   --option trusted-public-keys  "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY= lantian:EeAUQ+W+6r7EtwnmYjeVwx5kOGEBpjlBfPlzGlTNvHc= cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
 
 # ── 6. Persist config on installed system ─────────────────────────────────────
-sudo cp -rT "$WORK_DIR" ~/nixconf
+sudo cp -rT "$WORK_DIR" /mnt/etc/nixos
 
 echo "==> Done! Rebooting..."
 sudo reboot
