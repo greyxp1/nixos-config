74ead7dd3005445b6ee3f7efcc90f6c460872857
greyxp1 <greyxp999@gmail.com>
2026-05-10 14:26:18 -0400

    added the new scripts and removed nirimod

--- modules/apps/niri/niri.nix
+++ modules/apps/niri/niri.nix
@@ -3,18 +3,29 @@
   flake.nixosModules.niri =
     { pkgs, ... }:
     {
-      imports = [ inputs.niri.nixosModules.niri ];
+      imports = [
+        inputs.niri.nixosModules.niri
+        inputs.niri-autoselect-portal.nixosModules.default
+      ];
 
       programs.niri = {
         enable = true;
         package = inputs.niri.packages.${pkgs.stdenv.hostPlatform.system}.niri-unstable;
       };
 
+      services.niri-autoselect-portal.enable = true;
+      xdg.portal.extraPortals = [ pkgs.xdg-desktop-portal-gtk ];
+
       home-manager.sharedModules = [
         {
           programs.niri.config = builtins.readFile ./config.kdl;
-          home.packages = [ inputs.nirimod.packages.${pkgs.stdenv.hostPlatform.system}.default ];
-          home.sessionVariables.NIRIMOD_CONFIG_DIR = "$HOME/nixconf/modules/apps/niri";
+          home.packages = [
+            inputs.niri-float-sticky.packages.${pkgs.stdenv.hostPlatform.system}.default
+            (pkgs.writeShellScriptBin "screencast-monitor" (builtins.readFile ./screencast-monitor.sh))
+            (pkgs.writeShellScriptBin "tile-to-2" ''
+              exec ${pkgs.python3}/bin/python3 ${./tile-to-2.py} -n 2 -m "$@"
+            '')
+          ];
         }
       ];
     };
