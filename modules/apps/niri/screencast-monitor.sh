a5ec5a4cf78641a8961ceeea7d2a7cc179e4fccd
greyxp1 <greyxp999@gmail.com>
2026-05-13 19:28:46 -0400

    improved structure

--- modules/apps/niri/screencast-monitor.sh
+++ modules/apps/niri/screencast-monitor.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
-dbus-monitor --session "type='method_call',interface='org.freedesktop.portal.ScreenCast',member='Start'" 2>/dev/null | \
-awk '/method call/ {
-    system("niri msg action set-dynamic-cast-monitor")
-    fflush()
-}'
+dbus-monitor --session \
+    "type='method_call',interface='org.freedesktop.portal.ScreenCast',member='Start'" \
+    2>/dev/null |
+awk '/method call/ { system("niri msg action set-dynamic-cast-monitor"); fflush() }'
