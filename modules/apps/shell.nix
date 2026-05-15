1c95ab018815619e590d3b730c71af57f46873ff
greyxp1 <greyxp999@gmail.com>
2026-05-15 17:04:51 -0400

    moved from fish to nushell

--- modules/apps/shell.nix
+++ modules/apps/shell.nix
@@ -3,8 +3,6 @@
   flake.nixosModules.shell =
     { ... }:
     {
-      programs.fish.enable = true;
-
       home-manager.users.grey =
         { ... }:
         {
@@ -18,14 +16,15 @@
             };
           };
 
-          programs.fish = {
+          programs.nushell = {
             enable = true;
-            interactiveShellInit = "set -g fish_greeting";
 
-            functions = {
+            extraConfig = "$env.config.show_banner = false";
+
+            shellAliases = {
               rebuild = "nh os switch";
               update = "nh os switch --update";
-              hmswitch = "sudo systemctl restart home-manager-grey.service";
+              home = "sudo systemctl restart home-manager-grey.service";
             };
           };
         };
