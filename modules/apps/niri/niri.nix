a7f7b13c14f758abf5321c011d4f4db4947071f2
greyxp1 <greyxp999@gmail.com>
2026-05-15 17:51:19 -0400

    removed python script

--- modules/apps/niri/niri.nix
+++ modules/apps/niri/niri.nix
@@ -14,12 +14,7 @@
       };
 
       services.niri-autoselect-portal.enable = true;
-      xdg.portal.extraPortals = [
-        #pkgs.xdg-desktop-portal-gtk
-        pkgs.xdg-desktop-portal-termfilechooser
-      ];
-
-      # Route file picker requests to yazi via termfilechooser
+      xdg.portal.extraPortals = [ pkgs.xdg-desktop-portal-termfilechooser ];
       xdg.portal.config.niri."org.freedesktop.impl.portal.FileChooser" = [ "termfilechooser" ];
 
       home-manager.sharedModules = [
@@ -28,12 +23,8 @@
           home.packages = [
             inputs.niri-float-sticky.packages.${pkgs.stdenv.hostPlatform.system}.default
             (pkgs.writeShellScriptBin "screencast-monitor" (builtins.readFile ./screencast-monitor.sh))
-            (pkgs.writeShellScriptBin "tile-to-2" ''
-              exec ${pkgs.python3.interpreter} ${./tile-to-2.py} -n 2 -m -e "$@"
-            '')
           ];
 
-          # Configure termfilechooser to use yazi in kitty
           xdg.configFile."xdg-desktop-portal-termfilechooser/config".text = ''
             [filechooser]
             cmd=${pkgs.xdg-desktop-portal-termfilechooser}/share/xdg-desktop-portal-termfilechooser/yazi-wrapper.sh
