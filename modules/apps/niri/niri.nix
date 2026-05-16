fe50839931a371cd86d47b64fd38d1241f2b8ad6
greyxp1 <greyxp999@gmail.com>
2026-05-16 00:38:46 -0400

    baked in nushell screencast script with bash version commented out

--- modules/apps/niri/niri.nix
+++ modules/apps/niri/niri.nix
@@ -22,7 +22,23 @@
           programs.niri.config = builtins.readFile ./config.kdl;
           home.packages = [
             inputs.niri-float-sticky.packages.${pkgs.stdenv.hostPlatform.system}.default
-            (pkgs.writeShellScriptBin "screencast-monitor" (builtins.readFile ./screencast-monitor.sh))
+            (pkgs.writeScriptBin "screencast-monitor" ''
+              #!${pkgs.nushell}/bin/nu
+              dbus-monitor --session "type='method_call',interface='org.freedesktop.portal.ScreenCast',member='Start'"
+              | lines
+              | each { |line|
+                  if ($line | str contains "method call") {
+                    run-external "niri" "msg" "action" "set-dynamic-cast-monitor"
+                  }
+                }
+            '')
+
+            #(pkgs.writeShellScriptBin "screencast-monitor" ''
+            #  dbus-monitor --session \
+            #    "type='method_call',interface='org.freedesktop.portal.ScreenCast',member='Start'" \
+            #    2>/dev/null |
+            #  awk '/method call/ { system("niri msg action set-dynamic-cast-monitor"); fflush() }'
+            #'')
           ];
 
           xdg.configFile."xdg-desktop-portal-termfilechooser/config".text = ''
