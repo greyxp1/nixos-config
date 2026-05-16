c7d7cca2881526d92e8d2ea4d21c0cb1dc1e0ddd
greyxp1 <greyxp999@gmail.com>
2026-05-16 00:37:31 -0400

    added cli pkgs and configured shell

--- modules/apps/shell.nix
+++ modules/apps/shell.nix
@@ -1,13 +1,50 @@
 { ... }:
 {
   flake.nixosModules.shell =
-    { ... }:
+    { pkgs, ... }:
     {
+      programs.nh = {
+        enable = true;
+        flake = "/home/grey/nixconf";
+        clean = {
+          enable = true;
+          extraArgs = "--keep-since 2d --keep 3";
+        };
+      };
+
+      users.users.grey.shell = pkgs.nushell;
+
       home-manager.users.grey =
         { ... }:
         {
+          programs.nushell = {
+            enable = true;
+            environmentVariables.PAGER = "bat";
+            settings.show_banner = false;
+            shellAliases = {
+              rebuild = "nh os switch";
+              update = "nh os switch --update";
+              home = "sudo systemctl restart home-manager-grey.service";
+              clean = "nh clean all";
+              tree = "lstr";
+              cdi = "__zoxide_zi";
+            };
+          };
+
+          programs.zoxide = {
+            enable = true;
+            enableNushellIntegration = true;
+            options = [ "--cmd cd" ];
+          };
+
+          programs.carapace = {
+            enable = true;
+            enableNushellIntegration = true;
+          };
+
           programs.starship = {
             enable = true;
+            enableNushellIntegration = true;
             settings = {
               add_newline = false;
               aws.disabled = true;
@@ -16,17 +53,19 @@
             };
           };
 
-          programs.nushell = {
-            enable = true;
-
-            extraConfig = "$env.config.show_banner = false";
-
-            shellAliases = {
-              rebuild = "nh os switch";
-              update = "nh os switch --update";
-              home = "sudo systemctl restart home-manager-grey.service";
-            };
-          };
+          home.packages = with pkgs; [
+            curl
+            lstr
+            bat
+            fastfetch
+            btop
+            zip
+            unzip
+            wget
+            codex
+            nerd-fonts.jetbrains-mono
+            fzf
+          ];
         };
     };
 }
