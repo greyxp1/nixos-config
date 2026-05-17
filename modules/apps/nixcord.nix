34e3b24507ba7b5289cd6272fcc99f55a115f384
greyxp1 <greyxp999@gmail.com>
2026-05-17 05:17:45 -0400

    added plugins and flags

--- modules/apps/nixcord.nix
+++ modules/apps/nixcord.nix
@@ -30,7 +30,8 @@
                 postFixup = (old.postFixup or "") + ''
                   wrapProgram $out/bin/equibop \
                     --add-flags "--force_high_performance_gpu" \
-                    --add-flags "--enable-features=VaapiVideoDecodeLinuxGL"
+                    --add-flags "--enable-features=VaapiVideoDecodeLinuxGL,AcceleratedVideoDecodeLinuxGL,AcceleratedVideoDecodeLinuxZeroCopyGL,AcceleratedVideoEncoder,VaapiOnNvidiaGPUs,VaapiIgnoreDriverChecks" \
+                    --add-flags "--ignore-gpu-blocklist"
                 '';
               });
             };
@@ -54,42 +55,62 @@
                 betterCommands.enable = true;
                 betterSettings.enable = true;
                 betterUploadButton.enable = true;
+                blockKrisp.enable = true;
                 ClearURLs.enable = true;
+                consoleJanitor.enable = true;
+                copyFileContents.enable = true;
+                copyStickerLinks.enable = true;
+                crashHandler.enable = true;
                 disableCallIdle.enable = true;
-                equibopStreamFixes.enable = true;
                 expressionCloner.enable = true;
                 fakeNitro.enable = true;
+                fixCodeblockGap.enable = true;
                 fixFileExtensions.enable = true;
                 fixImagesQuality.enable = true;
+                fixYoutubeEmbeds.enable = true;
                 fullSearchContext.enable = true;
                 FullVCPFP.enable = true;
+                gifPaste.enable = true;
                 guildPickerDumper.enable = true;
                 hideMessages.enable = true;
+                homeTyping.enable = true;
                 keepCurrentChannel.enable = true;
                 memberCount.enable = true;
                 messageClickActions.enable = true;
                 messageTranslate.enable = true;
                 micLoopbackTester.enable = true;
+                moreUserTags.enable = true;
                 newPluginsManager.enable = true;
+                noDevtoolsWarning.enable = true;
+                noF1.enable = true;
                 noMiddleClickPaste.enable = true;
+                noMosaic.enable = true;
                 noNitroUpsell.enable = true;
                 noOnboardingDelay.enable = true;
+                noPushToTalk.enable = true;
                 noTypingAnimation.enable = true;
                 noUnblockToJump.enable = true;
                 OnePingPerDM.enable = true;
                 pinIcon.enable = true;
                 platformIndicators.enable = true;
                 previewMessage.enable = true;
+                reactErrorDecoder.enable = true;
                 relationshipNotifier.enable = true;
                 remixRevived.enable = true;
                 reverseImageSearch.enable = true;
                 roleColorEverywhere.enable = true;
+                searchFix.enable = true;
                 showAllMessageButtons.enable = true;
+                sendTimestamps.enable = true;
                 showTimeoutDuration.enable = true;
+                stickerPaste.enable = true;
+                unindent.enable = true;
                 userVoiceShow.enable = true;
-                voiceRejoin.enable = true;
+                voiceChannelLog.enable = true;
                 webScreenShareFixes.enable = true;
+                whoReacted.enable = true;
                 whosWatching.enable = true;
+                youtubeAdblock.enable = true;
                 zipPreview.enable = true;
 
                 questify = {
@@ -120,6 +141,28 @@
                   ignoreSelf = true;
                   messageLimit = 0;
                   saveImages = true;
+                };
+
+                streamingCodecDisabler = {
+                  enable = true;
+                  #disableAv1Codec = true;
+                  disableH265Codec = true;
+                  disableVP9Codec = true;
+                  disableVP8Codec = true;
+                  disableH264Codec = true;
+                };
+
+                voiceRejoin = {
+                  enable = true;
+                  preventReconnectIfCallEnded = "none";
+                  rejoinDelay = 1.0;
+                  rejoinTimeout = 120.0;
+                };
+
+                equibopStreamFixes = {
+                  enable = true;
+                  minBitrate = 10000;
+                  bitsPerPixelPct = 16;
                 };
 
                 voiceMessages = {
